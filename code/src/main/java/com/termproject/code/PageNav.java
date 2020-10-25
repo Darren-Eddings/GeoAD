2cb1e08cad82d7b8eb2e3f3f025667819df63fd6
Darren-Eddings <62783075+darren-eddings@users.noreply.github.com>
2020-10-24 17:11:41 -0700

    added new module 'code' for the source code of our project

---
+++ code/src/main/java/com/termproject/code/PageNav.java
@@ -0,0 +1,13 @@
+package com.termproject.code;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageNav extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_nav);
+    }
+}
