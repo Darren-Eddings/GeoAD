598dfd270b1a76dc2954e69b0cdadf00ba62175b
Kieffer Juzenta Liestyo <Kieffer.Liestyo@mytwu.ca>
2020-11-03 19:16:12 -0800

    Updated Project

--- app/src/main/java/com/termproject/geoad/CaretakerAddPatient.java
+++ app/src/main/java/com/termproject/geoad/CaretakerAddPatient.java
@@ -24,6 +24,7 @@ public class CaretakerAddPatient extends Fragment implements View.OnClickListene
         cancel = view.findViewById(R.id.cancelButton);
         add.setOnClickListener(this);
         cancel.setOnClickListener(this);
+
         return view;
     }
 
