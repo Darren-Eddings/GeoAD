810e7de09d0031767f3bfbcb378660e2167bfb34
Kieffer Juzenta Liestyo <Kieffer.Liestyo@mytwu.ca>
2020-11-05 21:21:00 -0800

    Updated Project

--- app/src/main/java/com/termproject/geoad/PatientHomeScreen.java
+++ app/src/main/java/com/termproject/geoad/PatientHomeScreen.java
@@ -1,13 +1,13 @@
 package com.termproject.geoad;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
 
 public class PatientHomeScreen extends Fragment implements View.OnClickListener{
 
@@ -39,7 +39,8 @@ public class PatientHomeScreen extends Fragment implements View.OnClickListener{
 
         if (buttonId == R.id.mapButton){
 
-            newFragment = new MapFragment();
+            Intent intent = new Intent(getContext(), MapsActivity.class);
+            ((MainActivity) getActivity()).startActivity(intent);
 
         } else if (buttonId == R.id.emergencyButton) {
 
