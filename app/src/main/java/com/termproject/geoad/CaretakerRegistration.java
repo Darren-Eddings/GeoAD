4f876fbc09a90aa050eaa29c64d32e6ad1676236
Kieffer Juzenta Liestyo <Kieffer.Liestyo@mytwu.ca>
2020-11-23 17:53:47 -0800

    Updated Project

--- app/src/main/java/com/termproject/geoad/CaretakerRegistration.java
+++ app/src/main/java/com/termproject/geoad/CaretakerRegistration.java
@@ -2,29 +2,51 @@ package com.termproject.geoad;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CaretakerRegistration extends Fragment implements View.OnClickListener {
+
+    private FirebaseFirestore db;
+
+    private static final String TAG = "CaretakerRegistration";
 
     private Button registerButton;
     private TextView loginHere;
+    private EditText fullName;
+    private EditText dateOfBirth;
+    private EditText phone;
+    private EditText password;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+         db = FirebaseFirestore.getInstance();
 
         View view = inflater.inflate(R.layout.fragment_caretaker_registration, container, false);
+
         registerButton = view.findViewById(R.id.caretakerRegistrationButton);
         loginHere = view.findViewById(R.id.inkLogin);
+        fullName = view.findViewById(R.id.fullNameTextBox);
+        dateOfBirth = view.findViewById(R.id.dateOfBirthTextBox);
+        phone = view.findViewById(R.id.phoneTextBox);
+        password = view.findViewById(R.id.passwordTextBox);
+
         registerButton.setOnClickListener(this);
         loginHere.setOnClickListener(this);
 
@@ -32,7 +54,16 @@ public class CaretakerRegistration extends Fragment implements View.OnClickListe
     }
 
     @Override public void onClick(View v) {
+        Map<String, Object> caretaker = new HashMap<>();
+        caretaker.put("fullName", fullName.getText());
+        caretaker.put("dateOfBirth", dateOfBirth.getText());
+        caretaker.put("phone", phone.getText());
+        caretaker.put("password", password.getText());
 
+        db.collection("caretakers")
+                .add(caretaker)
+                .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId()))
+                .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
 
         Fragment nextFragment = null;
         int buttonId = v.getId();
