cc4cef5c0f4557629bbd786ff46ed90063a06142
Gideon Woo <gideonwoo@gmail.com>
2020-12-02 06:01:40 -0800

    Pointed GeofenceFieldManager function calls back to the CaretakerMapActivity.

--- app/src/main/java/com/termproject/geoad/GeofenceFieldManager.java
+++ app/src/main/java/com/termproject/geoad/GeofenceFieldManager.java
@@ -84,11 +84,11 @@ public class GeofenceFieldManager extends Fragment implements View.OnClickListen
             int radius = (int)size.getValue();
 
             //apply the attributes by calling functions from the activity
-            ((MapsActivity)getActivity()).setName(fenceName);
-            ((MapsActivity)getActivity()).setRadius(radius);
-            ((MapsActivity)getActivity()).drawGeofence(((MapsActivity)getActivity()).getLocation());
-            ((MapsActivity)getActivity()).addGeofence(((MapsActivity)getActivity()).getLocation());
-            ((MapsActivity)getActivity()).moveMap(((MapsActivity)getActivity()).getLocation());
+            ((CaretakerMapActivity)getActivity()).setName(fenceName);
+            ((CaretakerMapActivity)getActivity()).setRadius(radius);
+            ((CaretakerMapActivity)getActivity()).drawFence(((CaretakerMapActivity)getActivity()).getLocation());
+            ((CaretakerMapActivity)getActivity()).addGeofence(((CaretakerMapActivity)getActivity()).getLocation());
+            ((CaretakerMapActivity)getActivity()).moveMap(((CaretakerMapActivity)getActivity()).getLocation());
             getFragmentManager().beginTransaction().hide(this).commitAllowingStateLoss();
 
         }
